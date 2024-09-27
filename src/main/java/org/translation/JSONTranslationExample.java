3c412a68140c0886a0dabc8e308e782d9a8b1c78
3laine3 <elaine.liu3628@gmail.com>
2024-09-27 05:14:51 -0400

    finish todos up until 'the rest of TODOs'

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -40,9 +40,6 @@ public class JSONTranslationExample {
         return canada.getString("es");
     }
 
-    // TODO Task: Complete the method below to generalize the above to get the country name
-    //            for any country code and language code from sample.json.
-
     /**
      * Returns the name of the country based on the provided country and language codes.
      * @param countryCode the country, as its three-letter code.
@@ -50,6 +47,19 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        String countryName;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject country = jsonArray.getJSONObject(i);
+            if (country.getString("alpha3").equals(countryCode)) {
+                if (country.has(languageCode)) {
+                    countryName = country.getString(languageCode);
+                }
+                else {
+                    countryName = "Country not found";
+                }
+                return countryName;
+            }
+        }
         return "Country not found";
     }
 
